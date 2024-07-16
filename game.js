5360af98f36cfff9fa10c0f16295a0f5a97f3b2c
Alec Singer <51802911+Sosenteam@users.noreply.github.com>
2024-07-15 19:26:49 -0700

    Made core gameplay loop work! Notes fall, play noises when hit

--- game.js
+++ game.js
@@ -2,9 +2,9 @@
 First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
 
-@title: 
+@title: Rythym-Mania
 @author: 
-@tags: []
+@tags: []aa
 @addedOn: 2024-00-00
 */
 
@@ -23,23 +23,24 @@ function createKey(letter, melody, key) {
 }
 let key = [];
 key.push(new createKey("a", tune`
-300: F4~300,
+300: C4~300,
 9300`, "a"));
 key.push(new createKey("b", tune`
-300: G4~300,
+300: D4~300,
 9300`, "s"));
 key.push(new createKey("c", tune`
-300: A4~300,
+300: E4~300,
 9300`, "d"));
 key.push(new createKey("d", tune`
-300: B4~300,
+300: G4~300,
 9300`, "j"));
 key.push(new createKey("e", tune`
-300: C5~300,
+300: A4~300,
 9300`, "k"));
 key.push(new createKey("f", tune`
-300: D5~300,
+300: C5~300,
 9300`, "l"));
+
 
 setLegend(
   [note, bitmap`
@@ -161,54 +162,107 @@ setLegend(
 6666666666666666
 6666666666666666
 6666666666666666`],
+  ["y", bitmap`
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000
+..00000000000000`],
+  ["z", bitmap`
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...
+0000000000000...`]
 )
 
 setSolids([])
 
 const levels = [
   map`
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-..............
-.aabbccddeeff.`
+z......y
+z......y
+z......y
+z......y
+z......y
+z......y
+z......y
+z......y
+z......y
+z......y
+z......y
+zabcdefy
+z......y`
 ]
-
 setMap(levels[0])
+// console.log(getAll(key[0].s));
+for (let i = 0; i < key.length; i++) {
+  const currentKey = key[i];
 
-onInput("a", () => {
-  // Move the player one tile to the right
-   key[0].keyDown = true;
-})
-
-
-//Loopables
+  onInput(currentKey.key, () => {
+    currentKey.keyDown = true;
+    //console.log("KEY " + currentKey.key + " IS DOWN");
+    setTimeout(() => {
+      currentKey.keyDown = false;
+    }, 100);
+  });
+}
+setInterval(() => {
+  addSprite(getRandomInt(1, width() - 1), 0, note);
+}, 500);
+// Game loop
+// Game loop
 let gameLoop = setInterval(() => {
-  // Your logic here
-  console.log("LOOP");
+  let currentNotes = getAll(note);
+  currentNotes.forEach((item) => {
+    if (item.y >= height() - 1) {
+      item.remove();
+    }
+    item.y += 1;
+  });
   for (let i = 0; i < key.length; i++) {
-     console.log("KEY " + key[i].key + " IS " + key[i].keyDown );
+    let currentKeys = getAll(key[i].s);
     if (key[i].keyDown) {
-      playTune(key[i].melody);
+      currentKeys.forEach((item) => {
+        let checkTile = (x, y) => {
+          return getTile(x, y).some(obj => obj._type === "n") || getTile(x, y - 1).some(obj => obj._type === "n") || getTile(x, y - 2).some(obj => obj._type === "n");
+        };
+        if (checkTile(item.x, item.y)) {
+          playTune(key[i].melody);
+        }
+      });
     }
   }
-
-
-  afterInput(() => {
-    for (let i = 0; i < key.length; i++) {
-      key[i].keyDown = false;
-    }
-  });
-
 }, 100);
+
+
+
+//
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
